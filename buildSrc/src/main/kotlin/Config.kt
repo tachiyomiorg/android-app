70d09b59607b2fcb7a5749a6ff316d79794ec680
inorichi <len@kanade.eu>
2020-05-21 13:31:28 +0200

    Gradle wrapper update

--- buildSrc/src/main/kotlin/Config.kt
+++ buildSrc/src/main/kotlin/Config.kt
@@ -1,7 +1,7 @@
 object Config {
-  const val compileSdk = 29
   const val minSdk = 21
   const val targetSdk = 29
+  const val compileSdk = 29
   const val versionName = "1.0"
   const val versionCode = 1
   const val applicationId = "tachiyomi.app"
