576207c3c4d9731b810ee130148d0a353993fdc0
arkon <eugcheung94@gmail.com>
2020-09-04 12:35:04 -0400

    Update Gradle/AGP for AS 4.2 Canary 9

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ buildscript {
     jcenter()
   }
   dependencies {
-    classpath("com.android.tools.build:gradle:4.2.0-alpha01")
+    classpath("com.android.tools.build:gradle:4.2.0-alpha09")
     classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:${Deps.kotlin.version}")
     classpath("org.jetbrains.kotlin:kotlin-serialization:${Deps.kotlin.version}")
     classpath("com.jfrog.bintray.gradle:gradle-bintray-plugin:1.8.5")
