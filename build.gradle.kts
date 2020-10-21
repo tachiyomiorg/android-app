1cf62bfdd1cb2bdaa23e6f63291d9c8aa35f6c34
inorichi <len@kanade.eu>
2020-10-21 11:51:14 +0200

    Update coroutines to 1.4.0-M1. Use experimental stdlib api

--- build.gradle.kts
+++ build.gradle.kts
@@ -30,6 +30,7 @@ subprojects {
   tasks.withType<org.jetbrains.kotlin.gradle.dsl.KotlinJvmCompile> {
     kotlinOptions {
       freeCompilerArgs = freeCompilerArgs + listOf(
+        "-Xuse-experimental=kotlin.ExperimentalStdlibApi",
         "-Xuse-experimental=kotlinx.coroutines.FlowPreview",
         "-Xuse-experimental=kotlinx.coroutines.ExperimentalCoroutinesApi",
         "-Xuse-experimental=kotlinx.serialization.ExperimentalSerializationApi",
