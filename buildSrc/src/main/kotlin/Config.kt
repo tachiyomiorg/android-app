1798bddbcb119fad17b54b4b6cf813e9c4c00dbd
arkon <eugcheung94@gmail.com>
2020-11-27 22:09:56 -0500

    Bumping target/compile SDK to 30
    This should force us to consider the new Scoped Storage nonsense during development.

--- buildSrc/src/main/kotlin/Config.kt
+++ buildSrc/src/main/kotlin/Config.kt
@@ -1,7 +1,7 @@
 object Config {
   const val minSdk = 21
-  const val targetSdk = 29
-  const val compileSdk = 29
+  const val targetSdk = 30
+  const val compileSdk = 30
   const val ndk = "21.3.6528147"
   const val versionName = "1.0"
   const val versionCode = 1
