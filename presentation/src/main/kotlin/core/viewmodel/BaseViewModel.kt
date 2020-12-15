4816637110bfd5c2772e18b911947fc307d23b6a
inorichi <len@kanade.eu>
2020-12-15 21:40:47 +0100

    Add a method in view models to convert a flow to compose's State

--- presentation/src/main/kotlin/core/viewmodel/BaseViewModel.kt
+++ presentation/src/main/kotlin/core/viewmodel/BaseViewModel.kt
@@ -8,8 +8,14 @@
 
 package tachiyomi.ui.core.viewmodel
 
+import androidx.compose.runtime.State
+import androidx.compose.runtime.mutableStateOf
 import kotlinx.coroutines.MainScope
 import kotlinx.coroutines.cancel
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
 import tachiyomi.core.prefs.Preference
 import tachiyomi.ui.core.prefs.PreferenceMutableState
 
@@ -26,5 +32,21 @@ abstract class BaseViewModel {
   }
 
   fun <T> Preference<T>.asState() = PreferenceMutableState(this, scope)
+
+  fun <T> Flow<T>.asState(initialValue: T): State<T> {
+    val state = mutableStateOf(initialValue)
+    scope.launch {
+      collect { state.value = it }
+    }
+    return state
+  }
+
+  fun <T> StateFlow<T>.asState(): State<T> {
+    val state = mutableStateOf(value)
+    scope.launch {
+      collect { state.value = it }
+    }
+    return state
+  }
 
 }
