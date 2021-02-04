5866921b746fd54adefb7acc412748c8ef9f0a96
arkon <eugcheung94@gmail.com>
2021-02-03 23:21:49 -0500

    Update AGP and Gradle for Canary 5

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ buildscript {
     jcenter()
   }
   dependencies {
-    classpath("com.android.tools.build:gradle:7.0.0-alpha04")
+    classpath("com.android.tools.build:gradle:7.0.0-alpha05")
     classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:${Deps.kotlin.version}")
     classpath("org.jetbrains.kotlin:kotlin-serialization:${Deps.kotlin.version}")
     classpath("com.jfrog.bintray.gradle:gradle-bintray-plugin:1.8.5")
