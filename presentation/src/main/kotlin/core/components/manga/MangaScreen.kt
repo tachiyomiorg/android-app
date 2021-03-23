b2ba3c92a3d079229f2b3bbc70cb7c0393ea9be5
inorichi <len@kanade.eu>
2021-03-23 18:18:07 +0100

    Match manga header from tachiyomi

--- presentation/src/main/kotlin/core/components/manga/MangaScreen.kt
+++ presentation/src/main/kotlin/core/components/manga/MangaScreen.kt
@@ -12,22 +12,32 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.Button
+import androidx.compose.material.CircularProgressIndicator
+import androidx.compose.material.ContentAlpha
 import androidx.compose.material.Icon
+import androidx.compose.material.LocalContentColor
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.ProvideTextStyle
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
+import androidx.compose.material.TextButton
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.material.icons.filled.FavoriteBorder
+import androidx.compose.material.icons.filled.Public
+import androidx.compose.material.icons.filled.Sync
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
 import tachiyomi.domain.manga.model.Chapter
@@ -36,7 +46,7 @@ import tachiyomi.ui.core.coil.CoilImage
 import tachiyomi.ui.core.coil.MangaCover
 import tachiyomi.ui.core.components.BackIconButton
 import tachiyomi.ui.core.components.LoadingScreen
-import tachiyomi.ui.core.components.ScrollableColumn
+import tachiyomi.ui.core.components.SwipeToRefreshLayout
 import tachiyomi.ui.core.components.Toolbar
 
 @Composable
@@ -52,13 +62,34 @@ fun MangaScreen(
     return
   }
 
-  LazyColumn {
-    item {
-      MangaInfoHeader(navController, manga, onFavorite)
-    }
+  // TODO
+  val isRefreshing = false
+  val onRefresh = {}
+  val onTracking = {}
+  val onWebView = {}
 
-    items(chapters) { chapter ->
-      ChapterItem(chapter)
+  SwipeToRefreshLayout(
+    refreshingState = isRefreshing,
+    onRefresh = onRefresh,
+    refreshIndicator = {
+      Surface(elevation = 10.dp, shape = CircleShape) {
+        CircularProgressIndicator(
+          modifier = Modifier
+            .size(36.dp)
+            .padding(8.dp),
+          strokeWidth = 3.dp
+        )
+      }
+    }
+  ) {
+    LazyColumn(modifier = Modifier.fillMaxSize()) {
+      item {
+        MangaInfoHeader(navController, manga, onFavorite, onTracking, onWebView)
+      }
+
+      items(chapters) { chapter ->
+        ChapterItem(chapter)
+      }
     }
   }
 }
@@ -67,7 +98,9 @@ fun MangaScreen(
 private fun MangaInfoHeader(
   navController: NavHostController,
   manga: Manga,
-  onFavorite: () -> Unit
+  onFavorite: () -> Unit,
+  onTracking: () -> Unit,
+  onWebView: () -> Unit
 ) {
   val cover = MangaCover.from(manga)
   Column {
@@ -94,29 +127,57 @@ private fun MangaInfoHeader(
             CoilImage(model = cover)
           }
 
-          Column(modifier = Modifier.padding(start = 16.dp)) {
-            Text(manga.title, fontWeight = FontWeight.Bold)
-            if (manga.author.isNotBlank()) {
-              Text(manga.author)
+          Column(
+            modifier = Modifier
+              .padding(start = 16.dp, bottom = 16.dp)
+              .align(Alignment.Bottom)
+          ) {
+            Text(manga.title, style = MaterialTheme.typography.h6, maxLines = 3)
+
+            ProvideTextStyle(
+              MaterialTheme.typography.body2.copy(
+                color = LocalContentColor.current.copy(alpha = ContentAlpha.medium)
+              )
+            ) {
+              Text(manga.author, modifier = Modifier.padding(top = 4.dp))
+              if (manga.artist.isNotBlank() && manga.artist != manga.author) {
+                Text(manga.artist)
+              }
+              Row(modifier = Modifier.padding(top = 4.dp)) {
+                Text(manga.status.toString())
+                Text("•", modifier = Modifier.padding(horizontal = 4.dp))
+                Text(manga.sourceId.toString())
+              }
             }
-            if (manga.artist.isNotBlank()) {
-              Text(manga.artist)
-            }
-            Text(manga.status.toString())
-            Text(manga.sourceId.toString())
           }
         }
+      }
+    }
 
-        // Action bar
-        Row(modifier = Modifier.padding(start = 16.dp, end = 16.dp)) {
-          Button(onClick = onFavorite) {
-            Icon(imageVector = if (manga.favorite) {
+    // Action bar
+    Row(modifier = Modifier.padding(horizontal = 16.dp)) {
+      TextButton(onClick = onFavorite, modifier = Modifier.weight(1f)) {
+        Column(horizontalAlignment = Alignment.CenterHorizontally) {
+          Icon(
+            imageVector = if (manga.favorite) {
               Icons.Default.Favorite
             } else {
               Icons.Default.FavoriteBorder
-            }, contentDescription = null)
-            Text(if (manga.favorite) "In library" else "Add to library")
-          }
+            }, contentDescription = null
+          )
+          Text(if (manga.favorite) "In library" else "Add to library")
+        }
+      }
+      TextButton(onClick = onTracking, modifier = Modifier.weight(1f)) {
+        Column(horizontalAlignment = Alignment.CenterHorizontally) {
+          Icon(imageVector = Icons.Default.Sync, contentDescription = null)
+          Text("Tracking")
+        }
+      }
+      TextButton(onClick = onWebView, modifier = Modifier.weight(1f)) {
+        Column(horizontalAlignment = Alignment.CenterHorizontally) {
+          Icon(imageVector = Icons.Default.Public, contentDescription = null)
+          Text("WebView")
         }
       }
     }
