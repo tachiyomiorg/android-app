71406658fe9eaa9ea9b1c87e6ceabd547899c936
inorichi <len@kanade.eu>
2021-03-25 18:32:56 +0100

    Improve chapters UI

--- presentation/src/main/kotlin/manga/MangaScreen.kt
+++ presentation/src/main/kotlin/manga/MangaScreen.kt
@@ -8,14 +8,20 @@
 
 package tachiyomi.ui.manga
 
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
@@ -24,6 +30,7 @@ import androidx.compose.material.ButtonDefaults
 import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.material.ContentAlpha
 import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
 import androidx.compose.material.LocalContentColor
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.ProvideTextStyle
@@ -31,6 +38,8 @@ import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.material.TextButton
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowDownward
+import androidx.compose.material.icons.filled.Check
 import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.material.icons.filled.FavoriteBorder
 import androidx.compose.material.icons.filled.Public
@@ -38,6 +47,9 @@ import androidx.compose.material.icons.filled.Sync
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.AnnotatedString
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
 import tachiyomi.domain.manga.model.Chapter
@@ -49,6 +61,9 @@ import tachiyomi.ui.core.components.LoadingScreen
 import tachiyomi.ui.core.components.SwipeToRefreshLayout
 import tachiyomi.ui.core.components.Toolbar
 import tachiyomi.ui.core.viewmodel.viewModel
+import java.text.SimpleDateFormat
+import java.util.Date
+import java.util.Locale
 
 @Composable
 fun MangaScreen(
@@ -99,7 +114,7 @@ fun MangaScreen(
       }
 
       items(vm.chapters) { chapter ->
-        ChapterItem(chapter)
+        ChapterRow(chapter, false, {})
       }
     }
   }
@@ -214,7 +229,99 @@ private fun MangaInfoHeader(
   }
 }
 
+private val dateFormat = SimpleDateFormat("dd/MM/yy", Locale.ENGLISH)
+
 @Composable
-private fun ChapterItem(chapter: Chapter) {
-  Text("${chapter.number}: ${chapter.name}")
+private fun ChapterRow(
+  chapter: Chapter,
+  isDownloaded: Boolean,
+  onClick: () -> Unit,
+  onDownloadClick: () -> Unit = {},
+  onDeleteClick: () -> Unit = {}
+) {
+  Row(
+    modifier = Modifier
+      .height(64.dp)
+      .clickable(onClick = onClick)
+      .padding(start = 16.dp),
+    verticalAlignment = Alignment.CenterVertically
+  ) {
+    Column(
+      modifier = Modifier.weight(1f),
+      verticalArrangement = Arrangement.spacedBy(4.dp)
+    ) {
+      Text(
+        chapter.name,
+        style = MaterialTheme.typography.body2,
+        color = LocalContentColor.current.copy(
+          alpha = if (chapter.read) ContentAlpha.disabled else ContentAlpha.high
+        )
+      )
+      val subtitleStr = buildAnnotatedString {
+        if (chapter.dateUpload > 0) {
+          append(dateFormat.format(Date(chapter.dateUpload)))
+        }
+        if (!chapter.read && chapter.progress > 0) {
+          if (length > 0) append(" • ")
+          append(
+            AnnotatedString(
+              (chapter.progress + 1).toString(),
+              SpanStyle(color = LocalContentColor.current.copy(alpha = ContentAlpha.disabled))
+            )
+          )
+        }
+        if (chapter.scanlator.isNotBlank()) {
+          if (length > 0) append(" • ")
+          append(chapter.scanlator)
+        }
+      }
+      Text(
+        subtitleStr,
+        style = MaterialTheme.typography.caption,
+        color = LocalContentColor.current.copy(
+          alpha = if (chapter.read) ContentAlpha.disabled else ContentAlpha.medium
+        )
+      )
+    }
+    if (isDownloaded) {
+      DownloadedIconButton(onDeleteClick)
+    } else {
+      DownloadIconButton(onDownloadClick)
+    }
+  }
+}
+
+@Composable
+private fun DownloadIconButton(onClick: () -> Unit) {
+  IconButton(onClick = onClick, modifier = Modifier.fillMaxHeight().width(56.dp)) {
+    Surface(
+      shape = CircleShape,
+      border = BorderStroke(2.dp, LocalContentColor.current.copy(alpha = ContentAlpha.disabled)),
+    ) {
+      Icon(
+        Icons.Default.ArrowDownward,
+        null,
+        Modifier
+          .size(22.dp)
+          .padding(2.dp),
+        LocalContentColor.current.copy(alpha = ContentAlpha.disabled)
+      )
+    }
+  }
+}
+
+@Composable
+private fun DownloadedIconButton(onClick: () -> Unit) {
+  IconButton(onClick = onClick, modifier = Modifier.fillMaxHeight().width(56.dp)) {
+    Surface(shape = CircleShape, color = LocalContentColor.current) {
+      Icon(
+        Icons.Default.Check,
+        null,
+        Modifier
+          .size(22.dp)
+          .padding(2.dp),
+        MaterialTheme.colors.surface
+      )
+    }
+  }
 }
