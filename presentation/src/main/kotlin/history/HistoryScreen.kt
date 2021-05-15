4eb90a7559db4eb1bdce3790c2ba451c40646377
Andreas <andreas.everos@gmail.com>
2021-05-15 21:51:56 +0200

    Add history and updates composables (#14)

--- presentation/src/main/kotlin/history/HistoryScreen.kt
+++ presentation/src/main/kotlin/history/HistoryScreen.kt
@@ -8,12 +8,35 @@
 
 package tachiyomi.ui.history
 
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.PlayArrow
+import androidx.compose.material.icons.outlined.Delete
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import tachiyomi.domain.manga.model.Manga
 import tachiyomi.ui.R
+import tachiyomi.ui.core.coil.rememberMangaCover
+import tachiyomi.ui.core.components.MangaListItem
+import tachiyomi.ui.core.components.MangaListItemColumn
+import tachiyomi.ui.core.components.MangaListItemImage
+import tachiyomi.ui.core.components.MangaListItemSubtitle
+import tachiyomi.ui.core.components.MangaListItemTitle
 import tachiyomi.ui.core.components.Toolbar
 
 @Composable
@@ -23,5 +46,50 @@ fun HistoryScreen(navController: NavController) {
       Toolbar(title = { Text(stringResource(R.string.history_label)) })
     }
   ) {
+  }
+}
+
+@Composable
+fun HistoryItem(
+  manga: Manga,
+  onClickItem: (Manga) -> Unit = { /* TODO */ },
+  onClickDelete: (Manga) -> Unit = { /* TODO */ },
+  onClickPlay: (Manga) -> Unit = { /* TODO */ }
+) {
+  MangaListItem(
+    modifier = Modifier
+      .clickable { onClickItem(manga) }
+      .height(80.dp)
+      .fillMaxWidth()
+      .padding(end = 4.dp),
+  ) {
+    MangaListItemImage(
+      modifier = Modifier
+        .fillMaxHeight()
+        .aspectRatio(3f / 4f)
+        .padding(start = 16.dp, top = 8.dp, bottom = 8.dp)
+        .clip(MaterialTheme.shapes.medium),
+      mangaCover = rememberMangaCover(manga)
+    )
+    MangaListItemColumn(
+      modifier = Modifier
+        .weight(1f)
+        .padding(start = 16.dp, end = 8.dp)
+    ) {
+      MangaListItemTitle(
+        text = manga.title,
+        maxLines = 2,
+        fontWeight = FontWeight.SemiBold
+      )
+      MangaListItemSubtitle(
+        text = "Ch. 69 - 10:59" // TODO
+      )
+    }
+    IconButton(onClick = { onClickDelete(manga) }) {
+      Icon(imageVector = Icons.Outlined.Delete, contentDescription = "")
+    }
+    IconButton(onClick = { onClickPlay(manga) }) {
+      Icon(imageVector = Icons.Filled.PlayArrow, contentDescription = "")
+    }
   }
 }
